ccb0b7727e1a1a85f3c6b73b38386a7b0009018b
domakkk <domaw95@wp.pl>
2023-03-16 20:24:03 +0100

    Login screen #1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import logoo from './logoo.png'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div class="App">
+     <img src= {logoo} class='logo1' alt='logo'width={500} height={500}/>
+      <span class="App-text" width={306} height={40}>
+        W celu rozpoczęcia pracy z aplikacją prosimy <br>
+       </br> o zarejestrowanie się lub zalogowanie.
+       </span>
+       <button type= "button" class='button' width={345} height={50}> Zarejestruj się
+       </button>
+        <span id="row">
+        <span class="linia"> </span>
+        <span class="lub">  lub </span>
+        <span class="linia"> </span>
+        </span>
+        <form action="#'" method="post">
+          <input type="text" name='login' id="login" width={345} height={41} placeholder="Login..."/>
+          <input type="password" name="password" id="haslo" width={345} height={41} placeholder="Hasło..."/>
+          <button type= "button" class='button_2' width={345} height={50}> Zaloguj się
+          </button>
+          </form>
+      </div>
   );
-}
+  };
 
 export default App;
